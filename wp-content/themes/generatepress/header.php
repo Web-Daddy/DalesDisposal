d0bbf23a3519803f015a402ddc70ab107309d87f
root <root@Debian-60-squeeze-64-minimal>
2018-07-28 16:03:09 +0300

    Changes

--- wp-content/themes/generatepress/header.php
+++ wp-content/themes/generatepress/header.php
@@ -19,6 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 </head>
 
 <body <?php generate_body_schema();?> <?php body_class(); ?>>
